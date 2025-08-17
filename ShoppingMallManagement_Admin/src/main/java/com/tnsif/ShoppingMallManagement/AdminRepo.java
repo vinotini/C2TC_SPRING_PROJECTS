6cf4b2d2ef9dda12e8a9640cdf1c7b13fbc7d474
Vinotini <146613025+vinotini@users.noreply.github.com>
2025-08-17 17:16:06 +0530

    Delete ShoppingMallManagement_Admin directory

--- ShoppingMallManagement_Admin/src/main/java/com/tnsif/ShoppingMallManagement/AdminRepo.java
+++
@@ -1,8 +0,0 @@
-package com.tnsif.ShoppingMallManagement;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepo extends JpaRepository<Admin, Integer> {
-
-}
